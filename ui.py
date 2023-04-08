f16e430e6ef74a62acd0c1a6181008844483e5d2
Jamma Beans <admin@jammabeans.com>
2023-04-07 23:28:23 -0600

    Add drop down menu network

--- ui.py
+++ ui.py
@@ -1,7 +1,13 @@
 from tkinter import *
 import time
 
+network_var = None
+eth_node_url_entry = None
+contract_address_entry = None
+chain_id_entry = None
+
 def create_ui(send_transactions_callback, stop_transactions_callback, toggle_key_visibility_callback, start_thread_callback):
+    global network_var, eth_node_url_entry, contract_address_entry, chain_id_entry
     root = Tk()
     root.title("Bulk XENft minter")
 
@@ -28,14 +34,14 @@ def create_ui(send_transactions_callback, stop_transactions_callback, toggle_key
     eth_node_url_label.grid(row=1, column=0, padx=padding_x, pady=padding_y)
     eth_node_url_entry = Entry(root, width=50)
     eth_node_url_entry.grid(row=1, column=1, padx=padding_x, pady=padding_y)
-    eth_node_url_entry.insert(0, 'https://mainnet.ethereumpow.org')  
+      
 
     # Create and place the contract address input field
     contract_address_label = Label(root, text="Contract Address:", bg='white')
     contract_address_label.grid(row=3, column=0, padx=padding_x, pady=padding_y)
     contract_address_entry = Entry(root, width=50)
     contract_address_entry.grid(row=3, column=1, padx=padding_x, pady=padding_y)
-    contract_address_entry.insert(0, '0x94d9E02D115646DFC407ABDE75Fa45256D66E043')
+    
 
     # Create and place the count input field
     count_label = Label(root, text="VUMs per XENft:", bg='white')
@@ -70,7 +76,21 @@ def create_ui(send_transactions_callback, stop_transactions_callback, toggle_key
     chain_id_label.grid(row=8, column=0, padx=padding_x, pady=padding_y)
     chain_id_entry = Entry(root, width=50)
     chain_id_entry.grid(row=8, column=1, padx=padding_x, pady=padding_y)
-    chain_id_entry.insert(0, '10001')
+    
+
+    network_label = Label(root, text="Network:", bg='white')
+    network_label.grid(row=5, column=3, padx=padding_x, pady=padding_y)
+
+    network_var = StringVar(root)
+    network_var.set('Ethereum')  # Set default value
+
+    networks = ['Ethereum', 'BSC', 'Polygon', 'Avalanche C', 'Ethereum PoW', 'Moonbeam', 'Evmos', 'Fantom']  # Add more networks here if needed
+    network_dropdown = OptionMenu(root, network_var, *networks)
+    network_dropdown.grid(row=6, column=3, padx=padding_x, pady=padding_y)
+    network_dropdown.config(bg='white')
+
+    # Update the input fields when the network_var changes
+    network_var.trace('w', update_inputs)
 
     # Create and place the start button that triggers the start_thread function
     start_button = Button(root, text="Start", command=lambda: start_thread_callback(status_label, transactions_count_label, total_gas_spent_label), bg='white')
@@ -121,6 +141,70 @@ def start_thread(send_transactions):
     stop_flag = False
     # Start a new thread for the send_transactions function
     threading.Thread(target=send_transactions).start()
+
+def update_inputs(*args):
+    global network_var, eth_node_url_entry, contract_address_entry, chain_id_entry
+    selected_network = network_var.get()
+    
+    if selected_network == 'Ethereum':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://mainnet.infura.io/v3/')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '1')
+    elif selected_network == 'BSC':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://bsc-dataseed.binance.org/')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x1Ac17FFB8456525BfF46870bba7Ed8772ba063a5')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '56')
+    elif selected_network == 'Polygon':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://rpc-mainnet.maticvigil.com/')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x726bB6aC9b74441Eb8FB52163e9014302D4249e5')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '137')
+    elif selected_network == 'Avalanche Mainnet':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://api.avax.network/ext/bc/C/rpc')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x94d9E02D115646DFC407ABDE75Fa45256D66E043')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '43114') 
+    elif selected_network == 'Ethereum PoW':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://mainnet.ethereumpow.org')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x94d9E02D115646DFC407ABDE75Fa45256D66E043')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '10001') 
+    elif selected_network == 'Moonbeam':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://rpc.api.moonbeam.network')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x94d9E02D115646DFC407ABDE75Fa45256D66E043')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '1284')
+    elif selected_network == 'Evmos':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://eth.bd.evmos.org:8545')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x4c4CF206465AbFE5cECb3b581fa1b508Ec514692')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '9001') 
+    elif selected_network == 'Fantom':
+        eth_node_url_entry.delete(0, END)
+        eth_node_url_entry.insert(0, 'https://rpc.ankr.com/fantom')
+        contract_address_entry.delete(0, END)
+        contract_address_entry.insert(0, '0x94d9E02D115646DFC407ABDE75Fa45256D66E043')
+        chain_id_entry.delete(0, END)
+        chain_id_entry.insert(0, '250')   
+      
+
+
 
 def run_ui(root):
     root.mainloop()
